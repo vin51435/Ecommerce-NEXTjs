5ba7b799b11eb1676a7b20c8e9c6c9c9db05da08
VIN <v3p51435@gmail.com>
2023-07-15 16:25:16 +0530

    Added Clerk authentication initial setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,13 @@
 // Home page of our app
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 
 const RootPage = () => {
   return (
-    <div className="m-14">
-      <Button size="default" variant="default">
-        Click me
-      </Button>
+    <div className="p-4">
+      This is a protected route!
+      <UserButton afterSignOutUrl='/'/>
     </div>
   );
 };
